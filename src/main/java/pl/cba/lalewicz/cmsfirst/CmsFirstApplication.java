786e71371c69ccc7e55d2f725b576f1ac442051b
Tomasz Lalewicz <lalewicz.tomasz@gmail.com>
2022-07-19 23:56:38 +0200

    second commit

---
+++ src/main/java/pl/cba/lalewicz/cmsfirst/CmsFirstApplication.java
@@ -0,0 +1,13 @@
+package pl.cba.lalewicz.cmsfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cba/lalewicz/cmsfirst/CmsFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cba.lalewicz.cmsfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
